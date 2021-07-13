015314390489c1b6ea321bc5706ff8e503db954e
Shoki16 <continuation.is.power800@gmail.com>
2021-07-13 15:58:06 +0900

    new

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ const arraynew=array.filter(function (x, i, self) { return self.indexOf(x) === i
 console.log(arraynew);
 
 
-function leap_Year(西暦) {
-  const year = [2020, 2021];
-  if (year / 4 === 0 && year / 100 !== 0 || year / 400 === 0) {
-    console.log(year + "年はうるう年です");
+function leapYear(year) {
+  if (year % 4 == 0 && year % 100 !== 0) {
+    return "うるう年です";
+  } else if (year % 400 === 0) {
+    return "うるう年です";
   } else {
-    console.log(year + "年はうるう年ではありません");
+    return "うるう年ではありません";
   }
-  return year;
 }
 
-console.log(leap_Year);
+console.log(leapYear(2020));
+console.log(leapYear(2021));
